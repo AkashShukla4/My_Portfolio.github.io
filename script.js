9edd68c151f2aeab1144beda22ab696607a468c0
Akash Shukla <akashshukla.8979@gmail.com>
2023-12-26 18:36:38 +0530

    Few project updates.

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-
-
 // ===================== Show Message after contacting =====================
 
+let contactMe = document.querySelector('[value = "Send Message"]');
 
-    let contactMe = document.querySelector('[value = "Send Message"]');
+contactMe.addEventListener("click", callBack);
 
-    contactMe.addEventListener('click', callBack);
-
-    function callBack(){
-        setTimeout(() => {alert('Thank You for Contacting Me!')}
-        ,800);
-    }
-
-
-  
-  
+function callBack() {
+  setTimeout(() => {
+    alert("Thank You for Contacting Me!");
+  }, 500);
+}
